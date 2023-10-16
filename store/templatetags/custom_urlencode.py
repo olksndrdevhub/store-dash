cd880b09a7fabdcf79f512e221b3a74a52b2b24d
Oleksandr Romaniuk <oleksandr.romaniuk@protonmail.com>
2023-10-16 13:59:37 +0300

    handle exeption on custom urlencode

--- store/templatetags/custom_urlencode.py
+++ store/templatetags/custom_urlencode.py
@@ -5,9 +5,11 @@ register = template.Library()
 
 @register.simple_tag()
 def custom_urlencode(base_url, **kwargs):
-    valid_kwargs = {k: v for k, v in kwargs.items() if v and str(v).strip() != ''}
-    print(valid_kwargs)
-    urlencoded_args = urlencode(valid_kwargs)
-    print(urlencoded_args)
-    return f'{base_url}?{urlencoded_args}'
+    try:
+        valid_kwargs = {k: v for k, v in kwargs.items() if v and str(v).strip() != ''}
+        urlencoded_args = urlencode(valid_kwargs)
+        return f'{base_url}?{urlencoded_args}'
+    except Exception as e:
+        print(e)
+        return base_url
     
